3def492541e33e896ad8d30701d54fab8d146360
Morya Breland Jr <morya.58@gmail.com>
2014-10-27 12:30:01 -0700

    INFO 343 10/27/2014 Classwork

---
+++ js/app.js
@@ -0,0 +1,69 @@
+"use strict";
+
+$(document).ready(function () {
+    var tiles = [];
+    var idx;
+    for (idx = 1; idx <= 32; ++idx) {
+        tiles.push({
+            tileNum: idx,
+            src: 'img/tile' + idx + '.jpg'
+
+        });
+    }
+
+    console.log(tiles);
+    var shuffledTiles = _.shuffle(tiles);
+    console.log(shuffledTiles);
+
+    var selectedTiles = shuffledTiles.slice(0,8);
+    console.log(selectedTiles);
+    var tilePairs = [];
+    _.forEach(selectedTiles, function (tile) {
+        tilePairs.push(_.clone(tile));
+        tilePairs.push(_.clone(tile));
+    });
+    tilePairs = _.shuffle(tilePairs);
+    console.log(tilePairs);
+
+    var gameBoard = $('#game-board');
+    var row = $(document.createElement('div'));
+    var img;
+    _.forEach(tilePairs, function (tile, elemIdx) {
+        if (elemIdx > 0 && 0 === elemIdx % 4) {
+            gameBoard.append(row);
+            row = $(document.createElement('div'));
+        }
+        img = $(document.createElement('img'));
+        img.attr({
+            src: 'img/tile-back.png',
+            alt: 'image of tile ' + tile.tileNum
+        });
+        img.data('tile', tile);
+        row.append(img);
+    });
+    gameBoard.append(row);
+    $('#game-board img').click(function () {
+       var img = $(this);
+       var tile = img.data('tile');
+       img.fadeOut(100, function () {
+           if(tile.flipped) {
+               img.attr('src', 'img/tile-back.png');
+           }
+           else {
+               img.attr('src', tile.src);
+           }
+           img.fadeIn(100);
+           tile.flipped = !tile.flipped;
+       }); // after fadeOut
+    }); //click of gameboard images
+
+    var startTime = _.now();
+    var timer = window.setInterval(function () {
+        var elapsedSeconds = Math.floor((_.now() - startTime) / 1000);
+        $('#elapsed-seconds').text(elapsedSeconds);
+
+        if(elapsedSeconds >= 10) {
+            window.clearInterval(timer);
+        }
+    }, 1000);
+});
